e132419a1d69c03ec7e8318c6077610631ca0524
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-02-11 13:22:15 +0000

    primo commit

--- app_000_base.py
+++ app_000_base.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return ('<h1>Hello, world!</h1>')
+def saluto():
+    return ('<h1>CIAO!</h1>')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
